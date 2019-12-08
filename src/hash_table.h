d5f97213785bb3bacf42ad04b9f8459c495df2ed
Raj Khare <seven.triangles@icloud.com>
2019-12-08 18:38:38 +0530

    Push

---
+++ src/hash_table.h
@@ -0,0 +1,24 @@
+#ifndef HASH_TABLE_H_
+#define HASH_TABLE_H_
+
+typedef struct
+{
+    char *key;
+    char *value;
+} ht_item;
+
+typedef struct
+{
+    int size;
+    int count;
+    int base_size;
+    ht_item **items;
+} ht_hash_table;
+
+ht_hash_table *ht_new();
+void ht_del_hash_table(ht_hash_table *ht);
+void ht_insert(ht_hash_table *ht, const char *key, const char *value);
+char *ht_search(ht_hash_table *ht, const char *key);
+void ht_delete(ht_hash_table *h, const char *key);
+
+#endif // HASH_TABLE_H_
